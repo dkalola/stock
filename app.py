40bd0a4e14cfa0680fd1445097d4a65ac74e82b8
Divyanshu <divyanshukalola88@gmail.com>
2023-07-15 10:02:13 -0500

    Initial update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import Flask, render_template
+import requests
+import yfinance as yf 
 
 
 # Create an instance of the Flask application
@@ -10,6 +12,12 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+def get_stock_data(TIKER):
+    stock = yf.Ticker(TIKER)
+    stock = stock.history(period="max")
+
+
+
 # Run the application if this script is executed directly
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3000)
+    app.run(host='0.0.0.0',port=3000,debug=True)
